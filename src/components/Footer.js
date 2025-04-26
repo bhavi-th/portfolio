146a8e6277a37835a677d6df6a35c01d2ad168aa
Bhavith <bhaviths15@gmail.com>
2025-04-26 19:50:57 +0530

    Added name to the footer section

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,12 +11,15 @@ import WhatsappInverted from "../assets/icon/whatsapp-inverted.svg";
 function Footer({ themeInverted }) {
     return (
         <footer>
-            <ul id="social-links" className="fade-elements">
-                <li><a href="https://github.com/bhavi-th" target="_blank" rel="noreferrer"><img src={themeInverted ? GithubInverted : Github} alt="social links" /></a></li>
-                <li><a href="https://www.linkedin.com/in/bhavith-s/" target="_blank" rel="noreferrer"><img src={themeInverted ? LinkedInInverted : LinkedIn} alt="social links" /></a></li>
-                <li><a href="mailto:bhaviths20@gmail.com" target="_blank" rel="noreferrer"><img src={themeInverted ? MailInverted : Mail} alt="social links" /></a></li>
-                <li><a href="https://wa.me/919901949627" target="_blank" rel="noreferrer"><img src={themeInverted ? WhatsappInverted : Whatsapp} alt="social links" /></a></li>
-            </ul>
+            <div>
+                <span id="name">Bhavith.S</span>
+                <ul id="social-links" className="fade-elements">
+                    <li><a href="https://github.com/bhavi-th" target="_blank" rel="noreferrer"><img src={themeInverted ? GithubInverted : Github} alt="social links" /></a></li>
+                    <li><a href="https://www.linkedin.com/in/bhavith-s/" target="_blank" rel="noreferrer"><img src={themeInverted ? LinkedInInverted : LinkedIn} alt="social links" /></a></li>
+                    <li><a href="mailto:bhaviths20@gmail.com" target="_blank" rel="noreferrer"><img src={themeInverted ? MailInverted : Mail} alt="social links" /></a></li>
+                    <li><a href="https://wa.me/919901949627" target="_blank" rel="noreferrer"><img src={themeInverted ? WhatsappInverted : Whatsapp} alt="social links" /></a></li>
+                </ul>
+            </div>
             <p>
                 &copy; 2025 Bhavith. All rights reserved
             </p>
